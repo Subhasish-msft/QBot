b83880c1662066f3fa26374958828b3cb6a7095a
Subhasish <56869764+Subhasish-msft@users.noreply.github.com>
2020-09-04 19:05:53 +0530

    Update environment.prod.ts

--- Source/DashboardTabApp/src/environments/environment.prod.ts
+++ Source/DashboardTabApp/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbotdemotesting.azurewebsites.net/api/Request/",
+    apiBaseUrl: "https://qbotdeploymentchaturthasp.azurewebsites.net/api/Request/",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
-        tenantId: "7520da05-6887-4d9d-9876-45bbf886ac78",
-        clientId: "62a0eea7-3c79-450e-8083-ab31364d45a3",
+        tenantId: "da47b64e-6c7b-47df-85bb-28e857716611",
+        clientId: "b3fc183c-8d00-41eb-b0a8-5d24cefe8b51",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
